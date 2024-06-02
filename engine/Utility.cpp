000f338ee3f2da40a02975fbe75311b77a31177c
Paulina-PC-Cichecka <s30547@pjwstk.edu.pl>
2024-06-02 16:48:35 +0200

    Added randomization

--- engine/Utility.cpp
+++ engine/Utility.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <random>
 
 #include "Utility.hpp"
 
@@ -7,4 +8,12 @@ auto normalized(sf::Vector2f vec) -> sf::Vector2f{
 
     auto newVec = sf::Vector2f(vec.x / length, vec.y / length);
     return newVec;
+}
+
+auto randomize(int const from, int const to) -> int {
+    static auto device = std::random_device();
+    static auto engine = std::default_random_engine(device());
+
+    auto distribution = std::uniform_int_distribution(from, to);
+    return distribution(engine);
 }
--- engine/Utility.hpp
+++ engine/Utility.hpp
@@ -2,3 +2,5 @@
 #include <SFML/System/Vector2.hpp>
 
 auto normalized(sf::Vector2f) -> sf::Vector2f;
+
+auto randomize(int const from, int const to) -> int;

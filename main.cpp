0482cbaab5587ef31e65f39454e1f13f5d91919c
Paulina-PC-Cichecka <s30547@pjwstk.edu.pl>
2024-06-02 23:01:43 +0200

    b e a u t y

--- main.cpp
+++ main.cpp
@@ -6,10 +6,13 @@
 #include "engine/Game.hpp"
 
 auto main() -> int {
-    auto window = sf::RenderWindow(sf::VideoMode::getDesktopMode(),
-                                   "The Binding of PJATK", sf::Style::Fullscreen);
-    // window.setFramerateLimit(60);
+    auto window = sf::RenderWindow(
+        sf::VideoMode::getDesktopMode(), "The Binding of PJATK", sf::Style::Fullscreen
+    );
+
     window.setVerticalSyncEnabled(true);
+    window.setMouseCursorVisible(false);
+
     auto game = Game(window);
 
     while (window.isOpen()) {
